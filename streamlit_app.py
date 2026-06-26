4df1a0ecb6751ba765f758f40b54c5a0f49cd01a
Andrew <thompbad@bc.edu>
2026-06-26 13:05:12 -0400

    Update final dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.markdown(
     <style>
         @media (min-width: 1100px) {
             .st-key-connected_chart_block {
-                transform: translateX(-135px);
+                transform: translateX(-105px);
                 width: calc(100% + 105px);
             }
 
